1d8a011717226b66c7f24941bd280607c58868d8
Md Khademul Islam Nahin <87036714+Nahin197@users.noreply.github.com>
2025-10-17 06:24:49 +0600

    some changes done

--- src/BookingTest.java
+++ src/BookingTest.java
@@ -25,7 +25,7 @@ public class BookingTest {
     @Test
     void getParkingSlotsTest(){
         assertNotNull(book.getParkingSlot(),"parking slot null");
-        assertEquals(pt, book.getParkingSlot(),"vehicle not match");
+        assertEquals(pt, book.getParkingSlot(),"parking slot not match");
     }
 
     @Test
@@ -50,31 +50,91 @@ public class BookingTest {
 
     @Test
     void  completeBookingTest(){
-      book.completeBooking();
-      assertEquals(BookingStatus.COMPLETED,book.getBookingStatus(),"booking status not match");
+        Booking book2 = new Booking(2, v, pt, s1, e1, 300.0);
+        book2.completeBooking();
+      assertEquals(BookingStatus.COMPLETED,book2.getBookingStatus(),"booking status not match");
     }
 
     @Test
     void  cancelBookingTest(){
-        book.cancelBooking();
-        assertEquals(BookingStatus.CANCELLED,book.getBookingStatus(),"booking status not match");
+        Booking book3 = new Booking(3, v, pt, s1, e1, 400.0);
+        book3.cancelBooking();
+        assertEquals(BookingStatus.CANCELLED,book3.getBookingStatus(),"booking status not match");
+    }
+
+    @Test
+    void mixtesting(){
+        Booking book3 = new Booking(3, v, pt, s1, e1, 400.0);
+        book3.cancelBooking();
+        book3.completeBooking();
+        assertNotEquals(BookingStatus.CANCELLED,book3.getBookingStatus(),"booking status not match");
     }
 
 
+    @Test
+    void toStringTesting() {
+        String expected = "Booking{" +
+                "bookingId=" + book.getBookingId() +
+                ", vehicle=" + v + "\n" +
+                ", parkingSlot=" + pt +
+                ", startTime=" + s1 +
+                ", endTime=" + e1 +
+                ", amount=" +book.getAmount() +
+                ", bookingStatus=" + book.getBookingStatus() +
+                "}";
+        assertNotNull(book,"book cant be null");
+        assertEquals(expected, book.toString(), "object output doesnot match");
+    }
 
+    @Test
+    void nullTestingOfParkingSlotObject() {
+        assertThrows(Exception.class, () -> {
+           Booking b= new Booking(5, v, null, s1, e1, 100.0);});
+    }
 
+    @Test
+    void nullVehicleObjectTest() {
+        assertThrows(Exception.class, () -> {
+            new Booking(4, null, pt, s1, e1, 200.0);
+        });
+    }
+    @Test
+    void negativeAmountTest() {
+        assertThrows(Exception.class, () -> {new Booking(5, v, pt, s1, e1, -20.0);});
+    }
+    @Test
+    void TimingTest() {
+        LocalDateTime start = LocalDateTime.of(2025, 10, 8, 12, 0);
+        LocalDateTime end = LocalDateTime.of(2025, 10, 8, 10, 0);
+        Booking b = new Booking(11, v, pt, start, end, 200.0);
+        assertTrue(b.getStartTime().isAfter(b.getEndTime()));
+    }
 
+    @Test
+    void TimingTest2() {
+        LocalDateTime start = LocalDateTime.of(2025,10,8,10,0);
+        LocalDateTime end = LocalDateTime.of(2025,10,8,10,0);
+        Booking b = new Booking(12, v, pt, start, end, 100);
+        assertEquals(start, b.getStartTime());
+        assertEquals(end, b.getEndTime());
 
+    }
 
+    @Test
+    void nullTimingTest() {
+        LocalDateTime start = null;
+        LocalDateTime end = null;
+        Booking b = new Booking(11, v, pt, start, end, 200.0);
+        assertThrows(Exception.class, () -> {new Booking(4, v, pt, s1, e1, 200.0);});
 
+    }
 
+    @Test
+    void zeroAmountTesting() {
 
+        assertThrows(Exception.class, () -> {new Booking(4, v, pt, s1, e1, 0.0);});
 
-
-
-
-
-
+    }
 
 
 
--- src/VehicleTest.java
+++ src/VehicleTest.java
@@ -7,7 +7,7 @@ public class VehicleTest {
 
     VehicleType vehicleType=VehicleType.CAR;
 
-    Wallet w=new Wallet();
+    Wallet w=new Wallet(20.0);
 
     Vehicle v=new Vehicle(1,vehicleType,w);
 
@@ -41,19 +41,79 @@ public class VehicleTest {
     public void vehicleWalletBalanceTest() {
         Vehicle v1=new Vehicle(1,vehicleType,200);
 
-        assertEquals(200,v1.wallet.getBalance(),"vehicle id is not correct");
+        assertEquals(200,v1.getWallet().getBalance(),"vehicle id is not correct");
 
 
     }
 
     @Test
     public void tosrtingTest() {
-      //  Vehicle v1=new Vehicle();
-       assertNotNull(v,"this object should not be null");
 
+
+           String ObjectOutput = "Vehicle{vehicleId=1, vehicleType=CAR, walletBalance=20.0}";
+            assertNotNull(v,"this object should not be null");
+            assertEquals(ObjectOutput,v.toString(), " object output is not correct");
+        }
+    @Test
+    public void VehicleNullWalletWithToStringTEst() {
+        Vehicle v2 = new Vehicle(2, vehicleType, null);
+        assertNull(v2.getWallet());
+        v2.toString();
+    }
+    @Test
+    public void nullVehicleTypeWithTosrtingTest() {
+        Vehicle v3 = new Vehicle(3, null, w);
+        assertNull(v3.getVehicleType());
+        v3.toString();
+    }
+
+    @Test
+    public void NegativeBalanceTest() {
+        assertThrows(Exception.class,()->new Vehicle(4, vehicleType, -20));
+    }
+
+    @Test
+    public void nullVehicleTypeTest() {
+        Vehicle v= new Vehicle(3, null, w);
+        System.out.println(v.getVehicleType());
+
+        assertThrows(Exception.class,()->new Vehicle(3, null, w));
+
+    }
+
+    @Test
+    public void NullVehicleWalletTest() {
+        Vehicle v= new Vehicle(3, vehicleType, null);
+        System.out.println(v.getWallet());
+
+        assertThrows(Exception.class,()->new Vehicle(2, vehicleType, null));
+    }
+
+    @Test
+    public void NullVehicleWalletGetterTest() {
+        Vehicle v= new Vehicle(3, vehicleType, null);
+
+        assertNull(v.getWallet());
+
+        assertThrows(Exception.class,()->v.getWallet());
+
+    }
+    @Test
+    public void nullVehicleTypeGetterTest() {
+        Vehicle v= new Vehicle(3, null, w);
+
+        assertNull(v.getVehicleType());
+
+        assertThrows(Exception.class,()->v.getVehicleType());
 
     }
 
 
 
+
+
 }
+
+
+
+
